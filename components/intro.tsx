d3ecf81d11393e22d3768d01b54648ed1574371d
Aarsh Muley <137747963+Aarshm26@users.noreply.github.com>
2024-01-24 23:10:23 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,14 +7,7 @@ const Intro = () => {
         Tatvani
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+        Sapere Aude
       </h4>
     </section>
   );
